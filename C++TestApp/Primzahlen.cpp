78b1c5a0b87c116646516deaab9c7147fd082183
Ben Sowieja <ben.sowieja@4sellers.de>
2025-09-10 16:27:27 +0200

    test

---
+++ C++TestApp/Primzahlen.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int inputNumber;
+	bool isPrime = true;
+
+	std::cout << "Geben Sie eine Zahl ein: ";
+	std::cin >> inputNumber;
+
+	if (inputNumber <= 1) {
+		isPrime = false; 
+	} else {
+		for (int i = 2; i <= inputNumber; i++) {
+			if (inputNumber % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+	}
+	if (isPrime) {
+		std::cout << inputNumber << " ist eine Primzahl." << std::endl;
+	} else {
+		std::cout << inputNumber << " ist keine Primzahl." << std::endl;
+	}
+	system("pause");
+}
---
+++ C++TestApp/Primzahlen.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <iomanip> // needed for setw
+
+using namespace std; //standard namespace
+
+//<< bedeutet: „schiebe in den Ausgabestrom“
+//>> bedeutet : „hole aus dem Eingabestrom“
+
+int main() {
+	
+}
