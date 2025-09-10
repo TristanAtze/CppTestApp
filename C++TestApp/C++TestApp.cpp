0f76e7c8936bf3a112375ca5584215156a1bb58b
Ben Sowieja <ben.sowieja@4sellers.de>
2025-09-10 16:11:22 +0200

    Added Test 2

--- C++TestApp/C++TestApp.cpp
+++ C++TestApp/C++TestApp.cpp
@@ -1,62 +1,62 @@
-#include <iostream>
-#include <string>
-using namespace std; //standard namespace
-
-//<< bedeutet: „schiebe in den Ausgabestrom“
-//>> bedeutet : „hole aus dem Eingabestrom“
-
-int main()
-{
-    int zahl;
-	bool continueProgram = true;
-	std::cout << "Geben Sie eine Zahl ein: "; // character output --> cout
-	std::cin >> zahl; // character input  --> cin
-	std::cout << "Die eingegebene Zahl ist: " << zahl << std::endl; // endl = end line
-
-
-	do
-	{
-		double a, b;
-		std::cout << "\n\n\n\n\nTaschenrechner in C++\n";
-		std::cout << "Geben die erste Zahle ein: ";
-		std::cin >> a;
-
-		std::cout << "Gebe die zweite Zahl ein: ";
-		std::cin >> b;
-
-		char op;
-		std::cout << "Geben Sie den Operator ein (+, -, *, /): ";
-		std::cin >> op;
-
-		switch (op)
-		{
-			case '+':
-				std::cout << "Ergebnis: " << a + b << std::endl;
-				break;
-			case '-':
-				std::cout << "Ergebnis: " << a - b << std::endl;
-				break;
-			case '*':
-				std::cout << "Ergebnis: " << a * b << std::endl;
-				break;
-			case '/':
-				if (b != 0)
-					std::cout << "Ergebnis: " << a / b << std::endl;
-				else
-					std::cout << "Fehler: Division durch Null!" << std::endl;
-				break;
-
-			default:
-				break;
-		}
-
-		std::cout << "Moechten Sie eine weitere Berechnung durchfuehren? (j/n): ";
-		char goOn;
-		std::cin >> goOn;
-		if (goOn == 'n' || goOn == 'N')
-			continueProgram = false;
-
-	} while (continueProgram);
-
-    return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std; //standard namespace
+//
+////<< bedeutet: „schiebe in den Ausgabestrom“
+////>> bedeutet : „hole aus dem Eingabestrom“
+//
+//int main()
+//{
+//    int zahl;
+//	bool continueProgram = true;
+//	std::cout << "Geben Sie eine Zahl ein: "; // character output --> cout
+//	std::cin >> zahl; // character input  --> cin
+//	std::cout << "Die eingegebene Zahl ist: " << zahl << std::endl; // endl = end line
+//
+//
+//	do
+//	{
+//		double a, b;
+//		std::cout << "\n\n\n\n\nTaschenrechner in C++\n";
+//		std::cout << "Geben die erste Zahle ein: ";
+//		std::cin >> a;
+//
+//		std::cout << "Gebe die zweite Zahl ein: ";
+//		std::cin >> b;
+//
+//		char op;
+//		std::cout << "Geben Sie den Operator ein (+, -, *, /): ";
+//		std::cin >> op;
+//
+//		switch (op)
+//		{
+//			case '+':
+//				std::cout << "Ergebnis: " << a + b << std::endl;
+//				break;
+//			case '-':
+//				std::cout << "Ergebnis: " << a - b << std::endl;
+//				break;
+//			case '*':
+//				std::cout << "Ergebnis: " << a * b << std::endl;
+//				break;
+//			case '/':
+//				if (b != 0)
+//					std::cout << "Ergebnis: " << a / b << std::endl;
+//				else
+//					std::cout << "Fehler: Division durch Null!" << std::endl;
+//				break;
+//
+//			default:
+//				break;
+//		}
+//
+//		std::cout << "Moechten Sie eine weitere Berechnung durchfuehren? (j/n): ";
+//		char goOn;
+//		std::cin >> goOn;
+//		if (goOn == 'n' || goOn == 'N')
+//			continueProgram = false;
+//
+//	} while (continueProgram);
+//
+//    return 0;
+//}
---
+++ C++TestApp/Test2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <iomanip> // needed for setw
+
+using namespace std; //standard namespace
+
+//<< bedeutet: „schiebe in den Ausgabestrom“
+//>> bedeutet : „hole aus dem Eingabestrom“
+
+int main() {
+	int zahl;
+	std::cout << "Gebe eine Zahl ein: ";
+
+	std::cin >> zahl;
+
+	for (int i = 1; i <= 10; i++) {
+		std::cout << zahl << " * " << i << " = " << zahl * i << std::endl;
+	}
+
+	std::cout << "Jetzt kommt die 1 - 10 Tabelle:\n";
+
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 10; j++) {
+			std::cout << i << " * " << j << " = " << i * j << "\t";
+		}
+	}
+	system("pause");
+	return 0;
+}
