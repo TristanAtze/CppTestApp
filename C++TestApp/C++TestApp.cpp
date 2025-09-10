410f267dd3e719ffccbf8550d059a494fbb9f861
Ben Sowieja <ben.sowieja@4sellers.de>
2025-09-10 15:12:02 +0200

    Dokumented

--- C++TestApp/C++TestApp.cpp
+++ C++TestApp/C++TestApp.cpp
@@ -1,15 +1,16 @@
-// C++TestApp.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
 #include <string>
+using namespace std; //standard namespace
+
+//<< bedeutet: „schiebe in den Ausgabestrom“
+//>> bedeutet : „hole aus dem Eingabestrom“
 
 int main()
 {
     int zahl;
-	std::cout << "Geben Sie eine Zahl ein: ";
-	std::cin >> zahl;
-	std::cout << "Die eingegebene Zahl ist: " << zahl << std::endl;
+	std::cout << "Geben Sie eine Zahl ein: "; // character output --> cout
+	std::cin >> zahl; // character input  --> cin
+	std::cout << "Die eingegebene Zahl ist: " << zahl << std::endl; // endl = end line
     system("pause");
     return 0;
 }
